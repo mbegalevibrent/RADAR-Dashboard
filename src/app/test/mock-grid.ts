1cecaedda7a1e1fc474bc5f6e4cf0485e78a346b
Maximilian Kerz <kerz.maximilian@gmail.com>
2017-01-24 14:54:36 +0000

    Stud view layout (mock-grid.json)

--- src/app/test/mock-grid.ts
+++ src/app/test/mock-grid.ts
@@ -1,16 +1,30 @@
 export const MockGrid = [
-  {
-    'cols': 3,
-    'id': 1,
-    'rows': 2,
-    'title': 'Heart Rate Monitoring',
-    'type': 'heart-rate'
-  },
-  {
-    'cols': 1,
-    'id': 2,
-    'rows': 1,
-    'title': 'TBD',
-    'type': 'empty'
-  }
+    {
+      "cols": 1,
+      "id": 1,
+      "rows": 1,
+      "title": "STUDY INFO",
+      "type": "empty"
+    },
+    {
+      "cols": 3,
+      "id": 2,
+      "rows": 1,
+      "title": "PATIENT & DEVICE MONITORING",
+      "type": "empty"
+    },
+    {
+      "cols": 1,
+      "id": 3,
+      "rows": 1,
+      "title": "PATEINT INFO",
+      "type": "empty"
+    },
+    {
+      "cols": 3,
+      "id": 4,
+      "rows": 1,
+      "title": "SENSOR MONITORING",
+      "type": "heart-rate"
+    }
 ];
